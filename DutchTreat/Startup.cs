3a506972827c21fbe211ea3d13646aa66b7e535d
Antonio Todorov <antoniorumtodorov@gmail.com>
2020-10-15 18:55:40 +0300

    Add index.html page
    When root path is blank index.html is returned

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Policy;
 
 namespace DutchTreat
 {
@@ -18,10 +19,10 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.Run(async context =>
-            {
-                await context.Response.WriteAsync("Hello World");
-            });
+            
+           app.UseDefaultFiles();
+            app.UseStaticFiles();
+            
         }
     }
 }
